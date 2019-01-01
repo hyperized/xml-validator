816494475870e5ca2a2853019e9261642c0594b5
Gerben Geijteman <gerben@hyperized.net>
2019-01-01 20:18:37 +0100

    Use correct tests namespace

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Hyperized\Xml\Validator\Tests;
+namespace Hyperized\Xml\Tests;
 
 use Hyperized\Xml\Constants\ErrorMessages;
 use Hyperized\Xml\Constants\Strings;
